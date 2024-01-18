8fa4b2e136097e3afa22c393f1f3cc3365afb2f4
Magdaly.santos <magdaly.santos@tenpo.cl>
2024-01-17 21:11:30 -0300

    add structure the proyect and router getAll and Save videos

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"net/http"
+	"golang/rest-api-gin/controller"
+	"golang/rest-api-gin/service"
 
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
 )
 
 type album struct {
@@ -15,10 +21,13 @@ type album struct {
 
 func main() {
 	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.IndentedJSON(http.StatusOK, gin.H{
-			"message": "OK!!",
-		})
+
+	router.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.FindAll())
+	})
+
+	router.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(201, VideoController.Save(ctx))
 	})
 
 	router.Run("localhost:8080")
